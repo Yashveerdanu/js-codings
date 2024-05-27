2d38b11c9d6e54d7d7a4e2421b1611307fbb086c
Yashveerdanu <yashveerdanu36@gmail.com>
2024-05-27 15:49:29 +0000

    second and third

---
+++ .devcontainer/01_basics/02_datatype.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as a newer version
+
+let age = 23
+let name = "yash"
+let decision = true
+
+//alert(4 + 4) //we are using nodejs not browser (only used in browser because of hidden js engine)  
+
+/* primitive datatypes
+--> number = 2 to power 53
+--> bigint
+--> string = ""
+--> boolean = true/false
+--> null = standalone value
+--> undefined;
+--> symbol = unique
+*/
+
+//object
+
+console.log(typeof decision);
+
+//null is a object (important)
+//undefined is undefined 
---
+++ .devcontainer/01_basics/conversionoperation.js
@@ -0,0 +1,22 @@
+let score = null
+console.log(typeof score)
+
+console.log(typeof (score))
+
+let stringtonumber = Number(score)
+
+console.log(typeof stringtonumber)
+console.log(stringtonumber)
+
+//"33" => 33
+//"33dsa" => NaN
+//true => 1 / false => 0
+
+
+let isloggedin = 1
+ let numbertoboolean = Boolean(isloggedin)
+ console.log(numbertoboolean); 
+
+ // 1 => true
+ //"" => false
+ //"yash" => true
