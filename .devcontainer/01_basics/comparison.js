87c433569bddab3af26a443f1c5bbdf32c985e90
dante <yashveerdanu36@gmail.com>
2024-05-28 16:40:53 +0000

    compare

---
+++ .devcontainer/01_basics/comparison.js
@@ -0,0 +1,23 @@
+//some basic comparision
+// console.log(2 > 1)
+// console.log(2 < 1)
+// console.log(2 >= 1)
+// console.log(2 <= 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+
+// console.log(null >  0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// try to avoid these type of comparison for better coding 
+// The reason is that an equality check == and comaprison > < >= <= work differentaly .comparison convert null a number treating it as a zero.
+
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+// console.log(undefined != 0);
+
+// strict check ===
+// consol.log("2" === 1)
