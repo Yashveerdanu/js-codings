4123b36fca650877b4d970fc403047ab61224057
dante <yashveerdanu36@gmail.com>
2024-05-29 18:16:43 +0000

    datatypes-summary

---
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -0,0 +1,21 @@
+// primitives
+
+// 7 types :   string,boolean,number,bigint,null,undefined,symbol
+
+// let  name = "hello" 
+// console.log(typeof name);
+
+// let decision = true
+// console.log(decision);
+// console.log(typeof decision);
+
+// let  value;
+// console.log(typeof value);
+
+// let bignumber =  49358724957295n
+// console.log(bignumber);
+// console.log(typeof bignumber);
+
+// refernce type or non-prmitive
+
+// arrays,objects, functions
