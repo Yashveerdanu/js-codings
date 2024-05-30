d7225845a92ad586de9b5835bd618fad08390c5e
dante <yashveerdanu36@gmail.com>
2024-05-30 06:55:03 +0000

    strings

---
+++ .devcontainer/01_basics/strings.js
@@ -0,0 +1,25 @@
+const name = "yashveer"
+const counts = 656
+// console.log(name + counts + " value");
+
+// string interpolation
+
+
+// console.log(`hello my name is ${name} and my count is ${counts}`);
+
+const gamename = new String('gow')
+console.log(gamename);
+
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+console.log(gamename.charAt(1));
+console.log(gamename.indexOf('w'));
+const newstring = gamename.substring(0,2)
+console.log(newstring);
+
+const anotherstring = gamename.slice(0,2)
+console.log(anotherstring);
+
+const newstring1 = '   yashudanu    '
+console.log(newstring1);
+console.log(newstring1.trim());
