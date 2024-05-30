97fe0cd6c22be9b20894a4752d42b55e2ce3ff3a
dante <yashveerdanu36@gmail.com>
2024-05-30 15:50:36 +0000

    number and  maths

---
+++ .devcontainer/01_basics/07_numbers_and_math.js
@@ -0,0 +1,34 @@
+// **********************************************NUMBER**********************************************************************
+
+// const score = 433
+// console.log(score);
+
+// const newscore = new Number(211)
+// console.log(newscore);
+
+// console.log(newscore.toString().length);
+// console.log(newscore.toFixed(2));
+
+// const othernumber = 434.45234
+// console.log(othernumber.toPrecision(3));
+
+// const nextnumber = 422533452
+// console.log(nextnumber.toLocaleString('en-In'));
+
+
+// ********************************************************MATHS*********************************************************
+
+console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(4.7));
+// console.log(Math.ceil(4.2));
+// console.log(Math.sqrt(25));
+
+// console.log(Math.min(4,3,6,5,1,2));
+console.log(Math.random());
+console.log((Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1)
+
+let min = 10
+let max = 20
+console.log(Math.floor(Math.random () * (max - min + 1)) + min);
